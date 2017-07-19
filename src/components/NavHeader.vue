0d3c2b518e81455e4c45e17d7e93b2b279d8fb2e
Olivia <604314292@qq.com>
2017-07-20 00:24:11 +0800

    添加登录模态框静态页面

--- src/components/NavHeader.vue
+++ src/components/NavHeader.vue
@@ -7,6 +7,7 @@
             <path class="path3" d="M27.514 25.594c-1.769 0-3.203 1.434-3.203 3.203s1.434 3.203 3.203 3.203c1.769 0 3.203-1.434 3.203-3.203s-1.434-3.203-3.203-3.203zM27.514 30.717c-1.060 0-1.92-0.86-1.92-1.92s0.86-1.92 1.92-1.92c1.060 0 1.92 0.86 1.92 1.92s-0.86 1.92-1.92 1.92z"></path>
             <path class="path4" d="M9.599 25.594c-1.769 0-3.203 1.434-3.203 3.203s1.434 3.203 3.203 3.203c1.769 0 3.203-1.434 3.203-3.203s-1.434-3.203-3.203-3.203zM9.599 30.717c-1.060 0-1.92-0.86-1.92-1.92s0.86-1.92 1.92-1.92c1.060 0 1.92 0.86 1.92 1.92s-0.86 1.92-1.92 1.92z"></path>
         </symbol>
+    
         <div class="navbar">
             <div class="navbar-left-container">
                 <a href="/">
@@ -30,8 +31,145 @@
                 </div>
             </div>
         </div>
+    
+        <!-- 登录模态框 start -->
+        <div class="md-modal modal-msg md-modal-transition">
+            <div class="md-modal-inner">
+                <div class="md-top">
+                    <div class="md-title">登录</div>
+                    <button class="md-close">关闭</button>
+                </div>
+                <div class="md-content">
+                    <div class="confirm-tips">
+                        <div class="error-wrap">
+                            <span class="error error-show" v-show="errorTip">用户名或者密码错误</span>
+                        </div>
+                        <ul>
+                            <li class="regi_form_input">
+                                <i class="icon IconPeople"></i>
+                                <input type="text" tabindex="1" name="loginname" v-model="userName" class="regi_login_input regi_login_input_left" placeholder="User Name" data-type="loginname">
+                            </li>
+                            <li class="regi_form_input noMargin">
+                                <i class="icon IconPwd"></i>
+                                <input type="password" tabindex="2" name="password" v-model="userPwd" class="regi_login_input regi_login_input_left login-input-no input_text" placeholder="Password">
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="login-wrap">
+                        <a href="javascript:;" class="btn-login" @click="login">登 录</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- 登录模态框 end -->
+    
+        <!-- 遮罩层 start -->
+        <div class="md-overlay"></div>
+        <!-- 遮罩层 end -->
+    
     </header>
 </template>
+
+<style>
+.header {
+    width: 100%;
+    background-color: white;
+    font-family: "moderat", sans-serif;
+    font-size: 16px;
+}
+
+.navbar {
+    display: flex;
+    justify-content: space-between;
+    align-content: center;
+    width: 100%;
+    height: 70px;
+    max-width: 1280px;
+    margin: 0 auto;
+    padding: 5px 20px 10px 20px;
+}
+
+.navbar-left-container {
+    display: flex;
+    justify-content: flex-start;
+    align-items: center;
+    margin-left: -20px;
+}
+
+
+.header a,
+.footer a {
+    color: #666;
+    text-decoration: none;
+}
+
+a {
+    -webkit-transition: color .3s ease-out;
+    transition: color .3s ease-out;
+}
+
+.navbar-right-container {
+    display: none;
+    justify-content: flex-start;
+    align-items: center;
+}
+
+.navbar-menu-container {
+    display: flex;
+    justify-content: flex-end;
+    align-items: center;
+    padding-top: 10px;
+}
+
+.navbar-link {
+    padding-left: 15px;
+}
+
+.navbar-cart-container {
+    position: relative;
+}
+
+.navbar-cart-count {
+    justify-content: center;
+    align-items: center;
+    position: absolute;
+    top: -9px;
+    right: -11px;
+    width: 20px;
+    border-radius: 10px;
+    color: white;
+    background-color: #eb767d;
+    font-size: 16px;
+    font-weight: bold;
+    text-align: center;
+}
+
+.navbar-cart-logo {
+    width: 25px;
+    height: 25px;
+    transform: scaleX(-1);
+}
+</style>
+
+<script>
+import './../assets/css/login.css'
+import axios from 'axios'
+
+export default {
+    data() {
+        return {
+            userName: '',
+            userPwd: '',
+            errorTip: ''
+        }
+    },
+    methods: {
+        login() {
+
+        }
+    }
+}
+</script>
 
 
 
